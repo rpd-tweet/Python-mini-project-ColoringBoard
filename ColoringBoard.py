988e75443fab58107cde8a2dc4a696b97ae45959
rpdstranger <60440808+rpd-tweet@users.noreply.github.com>
2020-02-23 00:42:17 +0530

    Add files via upload

---
+++ ColoringBoard.py
@@ -0,0 +1,561 @@
+import tkinter as tk
+
+#-----------Buttons----------
+def b1():
+    global myButton1
+    def on_enter(e):
+       myButton1['background'] = 'white'
+    def on_leave(e):
+       myButton1['background'] = 'blue'
+    myButton1 = tk.Button(root2,text="       ",height=2,width=4)
+    myButton1.grid(column=0,row=0)
+    myButton1.bind("<Enter>", on_enter)
+    myButton1.bind("<Leave>", on_leave)
+def b2():
+    global myButton2
+    def on_enter2(e):
+       myButton2['background'] = 'white'
+    def on_leave2(e):
+       myButton2['background'] = 'blue'
+    myButton2 = tk.Button(root2,text="       ",height=2,width=4)
+    myButton2.grid(column=0,row=1)
+    myButton2.bind("<Enter>", on_enter2)
+    myButton2.bind("<Leave>", on_leave2)
+def b3():
+    global myButton3
+    def on_enter3(e):
+       myButton3['background'] = 'white'
+    def on_leave3(e):
+       myButton3['background'] = 'blue'
+    myButton3= tk.Button(root2,text="       ",height=2,width=4)
+    myButton3.grid(column=0,row=2)
+    myButton3.bind("<Enter>", on_enter3)
+    myButton3.bind("<Leave>", on_leave3)
+def b4():
+    global myButton4
+    def on_enter4(e):
+       myButton4['background'] = 'white'
+    def on_leave4(e):
+       myButton4['background'] = 'blue'
+    myButton4= tk.Button(root2,text="       ",height=2,width=4)
+    myButton4.grid(column=1,row=0)
+    myButton4.bind("<Enter>", on_enter4)
+    myButton4.bind("<Leave>", on_leave4)
+def b5():
+    global myButton5
+    def on_enter5(e):
+       myButton5['background'] = 'white'
+    def on_leave5(e):
+       myButton5['background'] = 'blue'
+    myButton5= tk.Button(root2,text="       ",height=2,width=4)
+    myButton5.grid(column=1,row=1)
+    myButton5.bind("<Enter>", on_enter5)
+    myButton5.bind("<Leave>", on_leave5)
+def b6():
+    global myButton6
+    def on_enter6(e):
+       myButton6['background'] = 'white'
+    def on_leave6(e):
+       myButton6['background'] = 'blue'
+    myButton6= tk.Button(root2,text="       ",height=2,width=4)
+    myButton6.grid(column=1,row=2)
+    myButton6.bind("<Enter>", on_enter6)
+    myButton6.bind("<Leave>", on_leave6)
+def b7():
+    global myButton7
+    def on_enter7(e):
+       myButton7['background'] = 'white'
+    def on_leave7(e):
+       myButton7['background'] = 'blue'
+    myButton7= tk.Button(root2,text="       ",height=2,width=4)
+    myButton7.grid(column=2,row=0)
+    myButton7.bind("<Enter>", on_enter7)
+    myButton7.bind("<Leave>", on_leave7)
+def b8():
+    global myButton8
+    def on_enter8(e):
+       myButton8['background'] = 'white'
+    def on_leave8(e):
+       myButton8['background'] = 'blue'
+    myButton8= tk.Button(root2,text="       ",height=2,width=4)
+    myButton8.grid(column=2,row=1)
+    myButton8.bind("<Enter>", on_enter8)
+    myButton8.bind("<Leave>", on_leave8)
+def b9():
+    global myButton9
+    def on_enter9(e):
+       myButton9['background'] = 'white'
+    def on_leave9(e):
+       myButton9['background'] = 'blue'
+    myButton9= tk.Button(root2,text="       ",height=2,width=4)
+    myButton9.grid(column=2,row=2)
+    myButton9.bind("<Enter>", on_enter9)
+    myButton9.bind("<Leave>", on_leave9)
+def b10():
+    global myButton10
+    def on_enter10(e):
+       myButton10['background'] = 'white'
+    def on_leave10(e):
+       myButton10['background'] = 'blue'
+    myButton10= tk.Button(root2,text="       ",height=2,width=4)
+    myButton10.grid(column=3,row=0)
+    myButton10.bind("<Enter>", on_enter10)
+    myButton10.bind("<Leave>", on_leave10)
+def b11():
+    global myButton11
+    def on_enter11(e):
+       myButton11['background'] = 'white'
+    def on_leave11(e):
+       myButton11['background'] = 'blue'
+    myButton11= tk.Button(root2,text="       ",height=2,width=4)
+    myButton11.grid(column=3,row=1)
+    myButton11.bind("<Enter>", on_enter11)
+    myButton11.bind("<Leave>", on_leave11)
+def b12():
+    global myButton12
+    def on_enter12(e):
+       myButton2['background'] = 'white'
+    def on_leave12(e):
+       myButton12['background'] = 'blue'
+    myButton12= tk.Button(root2,text="       ",height=2,width=4)
+    myButton12.grid(column=3,row=2)
+    myButton12.bind("<Enter>", on_enter12)
+    myButton12.bind("<Leave>", on_leave12)
+def b13():
+    global myButton13
+    def on_enter13(e):
+       myButton13['background'] = 'white'
+    def on_leave13(e):
+       myButton13['background'] = 'blue'
+    myButton13= tk.Button(root2,text="       ",height=2,width=4)
+    myButton13.grid(column=4,row=0)
+    myButton13.bind("<Enter>", on_enter13)
+    myButton13.bind("<Leave>", on_leave13)
+def b14():
+    global myButton14
+    def on_enter14(e):
+       myButton14['background'] = 'white'
+    def on_leave14(e):
+       myButton14['background'] = 'blue'
+    myButton14= tk.Button(root2,text="       ",height=2,width=4)
+    myButton14.grid(column=4,row=1)
+    myButton14.bind("<Enter>", on_enter14)
+    myButton14.bind("<Leave>", on_leave14)
+def b15():
+    global myButton15
+    def on_enter15(e):
+       myButton15['background'] = 'white'
+    def on_leave15(e):
+       myButton15['background'] = 'blue'
+    myButton15= tk.Button(root2,text="       ",height=2,width=4)
+    myButton15.grid(column=4,row=2)
+    myButton15.bind("<Enter>", on_enter15)
+    myButton15.bind("<Leave>", on_leave15)
+def b16():
+    global myButton16
+    def on_enter16(e):
+       myButton16['background'] = 'white'
+    def on_leave16(e):
+       myButton16['background'] = 'blue'
+    myButton16= tk.Button(root2,text="       ",height=2,width=4)
+    myButton16.grid(column=5,row=0)
+    myButton16.bind("<Enter>", on_enter16)
+    myButton16.bind("<Leave>", on_leave16)
+def b17():
+    global myButton17
+    def on_enter17(e):
+       myButton17['background'] = 'white'
+    def on_leave17(e):
+       myButton17['background'] = 'blue'
+    myButton17= tk.Button(root2,text="       ",height=2,width=4)
+    myButton17.grid(column=5,row=1)
+    myButton17.bind("<Enter>", on_enter17)
+    myButton17.bind("<Leave>", on_leave17)
+def b18():
+    global myButton18
+    def on_enter18(e):
+       myButton18['background'] = 'white'
+    def on_leave18(e):
+       myButton18['background'] = 'blue'
+    myButton18= tk.Button(root2,text="       ",height=2,width=4)
+    myButton18.grid(column=5,row=2)
+    myButton18.bind("<Enter>", on_enter18)
+    myButton18.bind("<Leave>", on_leave18)
+def b19():
+    global myButton19
+    def on_enter19(e):
+       myButton19['background'] = 'white'
+    def on_leave19(e):
+       myButton19['background'] = 'blue'
+    myButton19= tk.Button(root2,text="       ",height=2,width=4)
+    myButton19.grid(column=6,row=0)
+    myButton19.bind("<Enter>", on_enter19)
+    myButton19.bind("<Leave>", on_leave19)  
+def b20():
+    global myButton20
+    def on_enter20(e):
+       myButton20['background'] = 'white'
+    def on_leave20(e):
+       myButton20['background'] = 'blue'
+    myButton20= tk.Button(root2,text="       ",height=2,width=4)
+    myButton20.grid(column=6,row=1)
+    myButton20.bind("<Enter>", on_enter20)
+    myButton20.bind("<Leave>", on_leave20)    
+def b21():
+    global myButton21
+    def on_enter21(e):
+       myButton21['background'] = 'white'
+    def on_leave21(e):
+       myButton21['background'] = 'blue'
+    myButton21= tk.Button(root2,text="       ",height=2,width=4)
+    myButton21.grid(column=6,row=2)
+    myButton21.bind("<Enter>", on_enter21)
+    myButton21.bind("<Leave>", on_leave21)
+def b22():
+    global myButton22
+    def on_enter22(e):
+       myButton22['background'] = 'white'
+    def on_leave22(e):
+       myButton22['background'] = 'blue'
+    myButton22= tk.Button(root2,text="       ",height=2,width=4)
+    myButton22.grid(column=0,row=3)
+    myButton22.bind("<Enter>", on_enter22)
+    myButton22.bind("<Leave>", on_leave22)
+    
+def b23():
+    global myButton23
+    def on_enter23(e):
+       myButton23['background'] = 'white'
+    def on_leave23(e):
+       myButton23['background'] = 'blue'
+    myButton23= tk.Button(root2,text="       ",height=2,width=4)
+    myButton23.grid(column=0,row=4)
+    myButton23.bind("<Enter>", on_enter23)
+    myButton23.bind("<Leave>", on_leave23)
+def b24():
+    global myButton24
+    def on_enter24(e):
+       myButton24['background'] = 'white'
+    def on_leave24(e):
+       myButton24['background'] = 'blue'
+    myButton24= tk.Button(root2,text="       ",height=2,width=4)
+    myButton24.grid(column=0,row=5)
+    myButton24.bind("<Enter>", on_enter24)
+    myButton24.bind("<Leave>", on_leave24)
+    
+def b25():
+    global myButton25
+    def on_enter25(e):
+       myButton25['background'] = 'white'
+    def on_leave25(e):
+       myButton25['background'] = 'blue'
+    myButton25= tk.Button(root2,text="       ",height=2,width=4)
+    myButton25.grid(column=1,row=3)
+    myButton25.bind("<Enter>", on_enter25)
+    myButton25.bind("<Leave>", on_leave25)
+def b26():
+    global myButton26
+    def on_enter26(e):
+       myButton26['background'] = 'white'
+    def on_leave26(e):
+       myButton26['background'] = 'blue'
+    myButton26= tk.Button(root2,text="       ",height=2,width=4)
+    myButton26.grid(column=1,row=4)
+    myButton26.bind("<Enter>", on_enter26)
+    myButton26.bind("<Leave>", on_leave26)
+def b27():
+    global myButton27
+    def on_enter27(e):
+       myButton27['background'] = 'white'
+    def on_leave27(e):
+       myButton27['background'] = 'blue'
+    myButton27= tk.Button(root2,text="       ",height=2,width=4)
+    myButton27.grid(column=1,row=5)
+    myButton27.bind("<Enter>", on_enter27)
+    myButton27.bind("<Leave>", on_leave27)
+
+
+def b28():
+    global myButton28
+    def on_enter28(e):
+       myButton29['background'] = 'white'
+    def on_leave28(e):
+       myButton28['background'] = 'blue'
+    myButton28= tk.Button(root2,text="       ",height=2,width=4)
+    myButton28.grid(column=2,row=3)
+    myButton28.bind("<Enter>", on_enter28)
+    myButton28.bind("<Leave>", on_leave28)
+def b29():
+    global myButton29
+    def on_enter29(e):
+       myButton29['background'] = 'white'
+    def on_leave29(e):
+       myButton29['background'] = 'blue'
+    myButton29= tk.Button(root2,text="       ",height=2,width=4)
+    myButton29.grid(column=2,row=4)
+    myButton29.bind("<Enter>", on_enter29)
+    myButton29.bind("<Leave>", on_leave29)
+def b30():
+    global myButton30
+    def on_enter30(e):
+       myButton30['background'] = 'white'
+    def on_leave30(e):
+       myButton30['background'] = 'blue'
+    myButton30= tk.Button(root2,text="       ",height=2,width=4)
+    myButton30.grid(column=2,row=5)
+    myButton30.bind("<Enter>", on_enter30)
+    myButton30.bind("<Leave>", on_leave30)
+def b31():
+    global myButton31
+    def on_enter31(e):
+       myButton31['background'] = 'white'
+    def on_leave31(e):
+       myButton31['background'] = 'blue'
+    myButton31= tk.Button(root2,text="       ",height=2,width=4)
+    myButton31.grid(column=3,row=3)
+    myButton31.bind("<Enter>", on_enter31)
+    myButton31.bind("<Leave>", on_leave31)
+def b32():
+    global myButton32
+    def on_enter32(e):
+       myButton32['background'] = 'white'
+    def on_leave32(e):
+       myButton32['background'] = 'blue'
+    myButton32= tk.Button(root2,text="       ",height=2,width=4)
+    myButton32.grid(column=3,row=4)
+    myButton32.bind("<Enter>", on_enter32)
+    myButton32.bind("<Leave>", on_leave32)
+def b33():
+    global myButton33
+    def on_enter33(e):
+       myButton33['background'] = 'white'
+    def on_leave33(e):
+       myButton33['background'] = 'blue'
+    myButton33= tk.Button(root2,text="       ",height=2,width=4)
+    myButton33.grid(column=3,row=5)
+    myButton33.bind("<Enter>", on_enter33)
+    myButton33.bind("<Leave>", on_leave33)
+
+def b34():
+    global myButton34
+    def on_enter34(e):
+       myButton34['background'] = 'white'
+    def on_leave34(e):
+       myButton34['background'] = 'blue'
+    myButton34= tk.Button(root2,text="       ",height=2,width=4)
+    myButton34.grid(column=4,row=3)
+    myButton34.bind("<Enter>", on_enter34)
+    myButton34.bind("<Leave>", on_leave34)
+def b35():
+    global myButton35
+    def on_enter35(e):
+       myButton35['background'] = 'white'
+    def on_leave35(e):
+       myButton35['background'] = 'blue'
+    myButton35= tk.Button(root2,text="       ",height=2,width=4)
+    myButton35.grid(column=4,row=4)
+    myButton35.bind("<Enter>", on_enter35)
+    myButton35.bind("<Leave>", on_leave35)
+def b36():
+    global myButton36
+    def on_enter36(e):
+       myButton36['background'] = 'white'
+    def on_leave36(e):
+       myButton36['background'] = 'blue'
+    myButton36= tk.Button(root2,text="       ",height=2,width=4)
+    myButton36.grid(column=4,row=5)
+    myButton36.bind("<Enter>", on_enter36)
+    myButton36.bind("<Leave>", on_leave36)
+def b37():
+    global myButton37
+    def on_enter37(e):
+       myButton37['background'] = 'white'
+    def on_leave37(e):
+       myButton37['background'] = 'blue'
+    myButton37= tk.Button(root2,text="       ",height=2,width=4)
+    myButton37.grid(column=5,row=3)
+    myButton37.bind("<Enter>", on_enter37)
+    myButton37.bind("<Leave>", on_leave37)
+def b38():
+    global myButton38
+    def on_enter38(e):
+       myButton38['background'] = 'white'
+    def on_leave38(e):
+       myButton38['background'] = 'blue'
+    myButton38= tk.Button(root2,text="       ",height=2,width=4)
+    myButton38.grid(column=5,row=4)
+    myButton38.bind("<Enter>", on_enter38)
+    myButton38.bind("<Leave>", on_leave38)
+def b39():
+    global myButton39
+    def on_enter39(e):
+       myButton39['background'] = 'white'
+    def on_leave39(e):
+       myButton39['background'] = 'blue'
+    myButton39= tk.Button(root2,text="       ",height=2,width=4)
+    myButton39.grid(column=5,row=5)
+    myButton39.bind("<Enter>", on_enter39)
+    myButton39.bind("<Leave>", on_leave39)
+def b40():
+    global myButton40
+    def on_enter40(e):
+       myButton40['background'] = 'white'
+    def on_leave40(e):
+       myButton40['background'] = 'blue'
+    myButton40= tk.Button(root2,text="       ",height=2,width=4)
+    myButton40.grid(column=6,row=3)
+    myButton40.bind("<Enter>", on_enter40)
+    myButton40.bind("<Leave>", on_leave40)
+def b41():
+    global myButton41
+    def on_enter41(e):
+       myButton41['background'] = 'white'
+    def on_leave41(e):
+       myButton41['background'] = 'blue'
+    myButton41= tk.Button(root2,text="       ",height=2,width=4)
+    myButton41.grid(column=6,row=4)
+    myButton41.bind("<Enter>", on_enter41)
+    myButton41.bind("<Leave>", on_leave41)
+    
+def b42():
+    global myButton42
+    def on_enter42(e):
+       myButton42['background'] = 'white'
+    def on_leave42(e):
+       myButton42['background'] = 'blue'
+    myButton42= tk.Button(root2,text="       ",height=2,width=4)
+    myButton42.grid(column=6,row=5)
+    myButton42.bind("<Enter>", on_enter42)
+    myButton42.bind("<Leave>", on_leave42)
+    
+def b43():
+    global myButton43
+    def on_enter43(e):
+       myButton43['background'] = 'white'
+    def on_leave43(e):
+       myButton43['background'] = 'blue'
+    myButton43= tk.Button(root2,text="       ",height=2,width=4)
+    myButton43.grid(column=0,row=6)
+    myButton43.bind("<Enter>", on_enter43)
+    myButton43.bind("<Leave>", on_leave43)
+def b44():
+    global myButton44
+    def on_enter44(e):
+       myButton44['background'] = 'white'
+    def on_leave44(e):
+       myButton44['background'] = 'blue'
+    myButton44= tk.Button(root2,text="       ",height=2,width=4)
+    myButton44.grid(column=1,row=6)
+    myButton44.bind("<Enter>", on_enter44)
+    myButton44.bind("<Leave>", on_leave44)
+  
+def b45():
+    global myButton45
+    def on_enter45(e):
+       myButton45['background'] = 'white'
+    def on_leave45(e):
+       myButton45['background'] = 'blue'
+    myButton45= tk.Button(root2,text="       ",height=2,width=4)
+    myButton45.grid(column=2,row=6)
+    myButton45.bind("<Enter>", on_enter45)
+    myButton45.bind("<Leave>", on_leave45)
+def b46():
+    global myButton46
+    def on_enter46(e):
+       myButton46['background'] = 'white'
+    def on_leave46(e):
+       myButton46['background'] = 'blue'
+    myButton46= tk.Button(root2,text="       ",height=2,width=4)
+    myButton46.grid(column=3,row=6)
+    myButton46.bind("<Enter>", on_enter46)
+    myButton46.bind("<Leave>", on_leave46)
+def b47():
+    global myButton47
+    def on_enter47(e):
+       myButton47['background'] = 'white'
+    def on_leave47(e):
+       myButton47['background'] = 'blue'
+    myButton47= tk.Button(root2,text="       ",height=2,width=4)
+    myButton47.grid(column=4,row=6)
+    myButton47.bind("<Enter>", on_enter47)
+    myButton47.bind("<Leave>", on_leave47)
+def b48():
+    global myButton48
+    def on_enter48(e):
+       myButton48['background'] = 'white'
+    def on_leave48(e):
+       myButton48['background'] = 'blue'
+    myButton48= tk.Button(root2,text="       ",height=2,width=4)
+    myButton48.grid(column=5,row=6)
+    myButton48.bind("<Enter>", on_enter48)
+    myButton48.bind("<Leave>", on_leave48)
+def b49():
+    global myButton49
+    def on_enter49(e):
+       myButton49['background'] = 'white'
+    def on_leave49(e):
+       myButton49['background'] = 'blue'
+    myButton49= tk.Button(root2,text="       ",height=2,width=4)
+    myButton49.grid(column=6,row=6)
+    myButton49.bind("<Enter>", on_enter49)
+    myButton49.bind("<Leave>", on_leave49)
+#----------End of Buttons---------
+
+#-----frst root page----------
+def start1():
+   global root
+   root=tk.Tk() 
+   root.title("Color me")
+   root.geometry("400x400")
+   root.resizable(1,1)     
+   label=tk.Label(root,text="Color Me !",bd=1,font="Times 30").place(x=200, y=100, anchor="center")
+   label1=tk.Label(root,text="\n\nPlay to win, But\nEnjoy the fun 😉 \n",font="Times 15").place(x=200, y=200, anchor="center")
+   b=tk.Button(root,text="Instructions",command=lambda: instructions()).place(x=200, y=300, anchor="center")
+   root.mainloop()
+  
+#-------instruction page------
+def instructions():
+   root.destroy()
+   global root1
+   root1=tk.Tk()
+   root1.title("Color me")
+   root1.geometry("400x400")
+   root1.resizable(1,1) 
+   label1=tk.Label(root1,text="Instructions",font="Times 25").place(x=200, y=50, anchor="center")
+   label1=tk.Label(root1,text="Move your cursor over board to color\n\nEnjoy coloring!!!",font="Times 15").place(x=200, y=175, anchor="center")
+   b=tk.Button(root1,text="Board",command=lambda: game()).place(x=175,y=250)
+   root1.mainloop()
+#--------game page---------
+def game():
+   root1.destroy()
+   global root2
+   root2=tk.Tk()   
+   root2.title("Find What?")
+   root2.geometry("275x350")
+   root2.resizable(1,1) 
+   start()   
+   b=tk.Button(root2,text="Close",height=2,width=4,command=lambda: exit()).grid(column=3,row=7)
+   root2.mainloop()
+#-------exit--------
+def exit():
+   root2.destroy()
+   global root3
+   root3=tk.Tk()   
+   root3.title("Find What?")
+   root3.geometry("400x400")
+   root3.resizable(1,1) 
+   label1=tk.Label(root3,text="See you next time !",font="Times 25").place(x=200, y=200, anchor="center")
+   root3.after(800,root3.destroy)
+   root3.mainloop()
+   
+#---------game fn start----------
+def start():
+    b1();b2();b3();b4();b5();b6();b7();b8();b9();b10();b11();
+    b12();b13();b14();b15();b16();b17();b18();b19();
+    b20();b21();b22();b23();b24();b25();b26();b27();
+    b28();b29();b30();b31();b32();b33();b34();b35();b36();b37();b38();b39();b40();b41();b42();b43();b44();b45();b46();b47();b48();b49(); 
+
+#----------main-------
+if __name__=="__main__":
+   start1()
+
